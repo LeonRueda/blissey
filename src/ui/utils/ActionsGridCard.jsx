0c484303505196015ee131c222b0d8350d934bb5
Leon Rueda <Phlammariont@gmail.com>
2018-04-01 23:24:49 -0500

    starting the new grid Component.

---
+++ src/ui/utils/ActionsGridCard.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const EditGridCard = props => <i className="material-icons" style={{width: '2rem'}}>mode_edit</i>
+
+export default EditGridCard
---
+++ src/ui/utils/DeleteGridCard.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const DeleteGridCard = props => <i className="material-icons" style={{width: '2rem'}} key={2}>delete</i>
+
+export default DeleteGridCard
